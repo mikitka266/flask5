0b29da85c184ec92b60544642d5526494f006922
mikitka266 <107499465+mikitka266@users.noreply.github.com>
2023-09-27 13:24:58 +0300

    Update db.py

--- db.py
+++ db.py
@@ -7,5 +7,15 @@ DATABASE_URL = settings.DATABASE_URL
 database = databases.Database(DATABASE_URL)
 metadata = sqlalchemy.Metadata()
 engine  = sqlalchemy.create_engine(DATABASE_URL)
+
+users = sqlalchemy.Table(
+"users",
+metadata,
+sqlalchemy.Column("id", sqlalchemy.Integer,
+primary_key=True),
+sqlalchemy.Column("name", sqlalchemy.String(32)),
+sqlalchemy.Column("email", sqlalchemy.String(128)),
+)
+
 metadata.create_all(engine)
 
